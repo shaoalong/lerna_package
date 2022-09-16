487898236699deab6f625f641ed004fe8dc6e9d1
shaoalong <1593439044@qq.com>
2022-09-16 10:03:28 +0800

    init

---
+++ packages/one/lib/one.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = one;
+const watermaker = require('@shaoalong/watermaker_salone')
+console.log('watermaker:', watermaker)
+function one() {
+    return "Hello from one";
+}
